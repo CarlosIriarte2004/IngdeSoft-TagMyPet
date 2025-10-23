fa0be56f925e3b42b47af8d25ca4dee31a313c42
Lucas-RodriguezAncieta <lucassantiago.lsra@gmail.com>
2025-10-23 10:23:10 -0400

    test(jest): caso inicial validarCampos devuelve false si hay campos vacíos

---
+++ src/registrar_usuario.test.js
@@ -0,0 +1,14 @@
+const { validarCampos } = require("../src/registrar_usuario.js");
+
+describe("validarCampos()", () => {
+  test("devuelve false si algún campo está vacío", () => {
+    const usuario = {
+      nombre: "",
+      correo: "lucas@mail.com",
+      contrasena: "12345",
+      telefono: "77777777",
+      fechaNacimiento: "2000-01-01"
+    };
+    expect(validarCampos(usuario)).toBe(false);
+  });
+});
