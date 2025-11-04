4c50e978ea1eb26598ec01a1031a951d61c6344e
Carlitos <carlos.iriarte@ucb.edu.bo>
2025-11-04 09:56:22 -0400

    (fix):Eliminacion de codigo innecesarios

--- cypress/e2e/registro.cy.js
+++ cypress/e2e/registro.cy.js
@@ -1,9 +1,6 @@
 describe("Registro de mascota (Datos básicos)", () => {
   beforeEach(() => {
-    // Asegúrate de tener Parcel sirviendo registro.html en este puerto
-    // npx parcel registro.html  (o npm start si agregaste varias páginas)
     cy.visit("http://localhost:1234/registro.html");
-    // Verificamos que realmente estemos en la página correcta
     cy.contains("Registrar Nueva Mascota").should("be.visible");
   });
 
@@ -11,7 +8,6 @@ describe("Registro de mascota (Datos básicos)", () => {
     cy.get("#formRegistroMascota").within(() => {
       cy.get("#nombre").type("Lucky");
       cy.get("#edad").type("3");
-      // especie vacía a propósito
       cy.get("#raza").type("Golden");
       cy.root().submit();
     });
@@ -19,17 +15,14 @@ describe("Registro de mascota (Datos básicos)", () => {
   });
 
   it("registra la mascota exitosamente y la muestra en la lista", () => {
-    // ⚠️ En Cypress, la foto NO es obligatoria (gracias al flag en registro.js)
     cy.get("#formRegistroMascota").within(() => {
       cy.get("#nombre").type("Lucky");
       cy.get("#edad").type("3");
       cy.get("#especie").select("Perro");
       cy.get("#raza").type("Golden Retriever");
-      // No adjuntamos #foto a propósito
       cy.root().submit();
     });
 
-    // Mensaje de éxito y render de la lista
     cy.contains("Mascota registrada exitosamente.").should("be.visible");
     cy.get("#listaMascotas")
       .should("contain.text", "Lucky")
